df0610bfc977115d701187b1a99f66593ebec182
Asad Ali Baloch <asadalibaloch059@gmail.com>
2020-03-19 01:22:20 +0500

    added modules for table and paginations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, MatSortModule, MatTableModule, MatCheckboxModule } from "@angular/material";
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BasicFormComponent } from './basic-form/basic-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { RoleviewComponent } from './components/roleview/roleview.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicFormComponent
+    BasicFormComponent,
+    RoleviewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     MatInputModule,
+    MatCheckboxModule,
     MatFormFieldModule,
     MatButtonModule,
     MatCardModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
     BrowserAnimationsModule
   ],
   providers: [],
