8fbe148659837a83b1773646b00c757aefb5f615
Asad Ali Baloch <asadalibaloch059@gmail.com>
2020-03-19 01:19:55 +0500

    created role table for user

---
+++ src/app/components/roleview/roleview.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
+export interface UserData {
+  checkbox: string;
+  id: string;
+  name: string;
+  description: string;
+}
+
+/** Constants used to fill up our data base. */
+
+const NAMES: string[] = [
+  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
+  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
+];
+
+/**
+ * @title Data table with sorting, pagination, and filtering.
+ */
+@Component({
+  selector: 'app-roleview',
+  templateUrl: './roleview.component.html',
+  styleUrls: ['./roleview.component.css']
+})
+export class RoleviewComponent implements OnInit {
+  displayedColumns: string[] = ['checkbox', 'id', 'name', 'description'];
+  dataSource: MatTableDataSource<UserData>;
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  constructor() {
+    // Create 100 users
+    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
+  }
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
+
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+
+  return {
+    checkbox: id.toString(),
+    id: id.toString(),
+    name: name,
+    description: Math.round(Math.random() * 100).toString()
+  };
+}
